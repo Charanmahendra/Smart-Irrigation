c7bd6e9a82c53e94a415efb5698ead104c4ed415
nithin <nithinkorukonda132@gmail.com>
2019-07-13 10:59:28 +0530

    Update Ultra sonic.ino

--- Ultra sonic.ino
+++ Ultra sonic.ino
@@ -1,4 +1,17 @@
-    digitalWrite(trigPin, LOW);
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+int distance;
+void setup()
+{
+ pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+ pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+ Serial.begin(9600); // Serial.begin(9600);
+}
+void loop()
+{
+
+ digitalWrite(trigPin, LOW);
     delay(200);
     digitalWrite(trigPin, HIGH);
     delay(200);
